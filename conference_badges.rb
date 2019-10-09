1982d4289c38f790da0b3f434c39a22fc7bd36d2
Melissa Christman <melchrstmn@gmail.com>
2019-10-09 02:10:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,10 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
-  array = []
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |attendee|
+    badges << "Hello, my name is #{attendee}."
+  end
+  badges
 end
